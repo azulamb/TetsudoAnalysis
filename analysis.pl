ab6a2b8cffa7adbce1079eabb0a10623da0eecd0
Hiroki <1127785+azulamb@users.noreply.github.com>
2014-04-24 12:25:55 +0900

    add script.

---
+++ analysis.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+#http://w3land.mlit.go.jp/ksj/jpgis/datalist/KsjTmplt-N02-v1_1.html
+&Analysis( 'N02-08.xml', 'station.csv', ',' );
+
+sub Analysis()
+{
+  my ( $file, $output, $split ) = ( @_ );
+
+  my %st;
+  my %cv;
+
+  if ( open( FILE, $file ) )
+  {
+    my $key = '';
+    my $mode = '';
+    my $line;
+    while ( <FILE> )
+    {
+      $line = $_;
+      if ( $line =~ /\t\<jps\:GM\_Point id\=\"(n\_[0-9]+)\"\>/ )
+      {
+        $key = $1;
+        $mode = 'GMP';
+      } elsif ( $line =~ /\t\<ksj\:EB03 / )
+      {
+        $mode = 'EB';
+      } elsif ( $line =~ /\t\<jps\:GM\_Curve id\=\"(c\_[0-9]+)">/ )
+      {
+        $mode = $1;
+        $key = $mode;
+      } elsif ( $line =~ /\t\<GM\_PointRef\.point idref\=\"(n\_[0-9]+)\"\/>/ )
+      {
+        push( @{ $cv{ $key } }, $1 );
+      }
+
+      if ( $mode eq 'GMP' )
+      {
+        if ( $line =~ /\<DirectPosition\.coordinate\>([0-9\.]+) ([0-9\.]+)\<\/DirectPosition\.coordinate\>/ )
+        {
+          ( $st{ $key }{ 'x' }, $st{ $key }{ 'y' } ) = ( $1, $2 );
+        }
+      } elsif ( $mode eq 'EB' )
+      {
+        if ( $line =~ /\<ksj\:LOC idref\=\"(c\_[0-9]+)\"\/\>/ )
+        {
+          $key = $1;
+        } elsif ( $line =~ /\<ksj\:STN\>(.+)\<\/ksj\:STN\>/ )
+        {
+          my $name = $1;
+          foreach ( @{ $cv{ $key } } )
+          {
+            $st{ $_ }{ 'name' } = $name;
+          }
+        }
+      }
+
+      if ( $line =~ /\t\<\/jps\:GM\_Point\>/ )
+      {
+        $mode = '';
+      }elsif ( $line =~ /\t\<\/ksj\:EB03\>/)
+      {
+        $mode = '';
+      }
+    }
+  }
+
+  open( FILE, "> $output" );
+  foreach( keys( %st ) )
+  {
+    if ( exists( $st{ $_ }{ 'name' } ) )
+    {
+      print FILE join( $split, sprintf( "%.5f", $st{ $_ }{ 'x' } ), sprintf( "%.5f", $st{ $_ }{ 'y' }) , $st{ $_ }{ 'name' } ) . "\n";
+    }
+  }
+  close( FILE );
+
+}
